bc3c351c752713ff9fa0a773ad452705993ea526
Void-Anvesha <anvesharastogi1717@gmail.com>
2025-08-10 19:21:19 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (5.85%) - LeetSync

--- 35-search-insert-position/search-insert-position.cpp
+++ 35-search-insert-position/search-insert-position.cpp
@@ -4,18 +4,17 @@ public:
         int n=nums.size();
         int low=0;
         int high=n-1;
-        int ans=n;
+        int ans=0;
         while(low<=high)
         {
             int mid=(low+high)/2;
-            if(nums[mid]>=target)
+            if(nums[mid]==target)return mid ;
+            else if(nums[mid]<target) 
             {
-                ans=mid;
-                high=mid-1;
-            }
-            else{
                 low=mid+1;
+                ans=mid+1;
             }
+            else high=mid-1;
         }
         return ans;
     }
