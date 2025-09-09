03ce36f1c730ad844515e4bc901060706e25c3a4
Void-Anvesha <anvesharastogi1717@gmail.com>
2025-09-09 22:34:36 +0530

    Time: 116 ms (47.00%) | Memory: 20.3 MB (37.46%) - LeetSync

--- 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
@@ -1,36 +1,22 @@
 class Solution {
 public:
-   int getMaxCnt(map<char,int>&mp)
-   {
-     int maxi=0;
-      for(auto it:mp)
-      {   int val=it.second;
-          maxi=max(maxi,val);
-      }
-      return maxi;
-   }
-
-   int getMinCnt(map<char,int> &mp){
-    int mini=INT_MAX;
-    for(auto it:mp)
-      {   int val=it.second;
-          mini=min(mini,val);
-      }
-      return mini;
-    
-   }
     int beautySum(string s) {
         int sumOfBeauty=0;
-        for(int i=0;i<s.length();i++)
-        { 
-            map<char,int>mp;
-            for(int j=i;j<s.length();j++)
+        unordered_map<char,int>mp;
+        for(int i=0;i<s.size();i++){
+            mp.clear();
+            for(int j=i;j<s.size();j++)
             {
                 mp[s[j]]++;
-                int beauty=getMaxCnt(mp)-getMinCnt(mp);
+                int maxi=INT_MIN;
+                int mini=INT_MAX;
+                for(auto& it:mp){
+                    maxi=max(maxi,it.second);
+                    mini=min(mini,it.second);
+                }
+                int beauty=maxi-mini;
                 sumOfBeauty+=beauty;
             }
-
         }
         return sumOfBeauty;
     }
