7b7cdd42ffb216acac6fbffe2b63ba8012c5e815
Void-Anvesha <anvesharastogi1717@gmail.com>
2025-09-13 09:47:00 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (92.17%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long long m=n;
+        double ans=1;
+        if(m<0)m=-m;
+        while(m>0){
+            if(m%2==1){
+                ans*=x;
+                m-=1;
+            }
+            else{
+                m=m/2;
+                x=x*x;
+            }
+        }
+        if(n<0){
+            ans=1.0/ans;
+            return ans;
+        }
+        return ans;
+    }
+};
