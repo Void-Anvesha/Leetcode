091471115268366600880765f6f952b215d1528b
Void-Anvesha <anvesharastogi1717@gmail.com>
2025-09-19 08:39:49 +0530

    Time: 0 ms (100.00%) | Memory: 52.3 MB (66.04%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -2,9 +2,10 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
         int maxIdx=0;
-        for(int i=0;i<nums.size();i++)
-        {
-            if(i>maxIdx) return false;
+        for(int i=0;i<nums.size();i++){
+            //Can't reach the end
+            if(i>maxIdx)return false;
+
             maxIdx=max(maxIdx,i+nums[i]);
         }
         return true;
