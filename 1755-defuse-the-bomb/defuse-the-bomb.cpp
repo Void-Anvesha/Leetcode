915cb6e3427c8f317d179ddbfd0804dac18dc5db
Void-Anvesha <anvesharastogi1717@gmail.com>
2025-10-31 19:14:56 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (18.29%) - LeetSync

---
+++ 1755-defuse-the-bomb/defuse-the-bomb.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+class Solution {
+public:
+    vector<int> decrypt(vector<int>& code, int k) {
+        int n=code.size();
+        vector<int>result(n);
+        if(k==0)return result;
+        int sum=0;
+
+        int start=1,end=k;
+        if(k<0){
+            start=n-abs(k);
+            end=n-1;
+        }
+
+        for(int i=start;i<=end;i++)sum+=code[i];
+
+        for(int i=0;i<n;i++){
+            result[i]=sum;
+            sum-=code[start%n];
+            sum+=code[(end+1)%n];
+            start++;
+            end++;
+        }
+        return result;
+
+
+
+    }
+};
