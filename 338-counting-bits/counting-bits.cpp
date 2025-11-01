0e3f6f050c3c4a37b9e54fd0e54f02ffea833f55
Void-Anvesha <anvesharastogi1717@gmail.com>
2025-11-01 21:28:06 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (57.44%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -1,11 +1,24 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int>result;
+        vector<int>result(n+1);
 
-        for(int i=0;i<=n;i++){
-            result.push_back(__builtin_popcount(i));
+        //Base Case
+        if(n==0) return result;
+        //Binary of 0 has 0 no of bits set as 1
+        result[0]=0;
+
+        for(int i=1;i<=n;i++){
+            if(i%2==1){
+                //Suppose 3: 11 & 3/2 is 1+1=2 
+                result[i]=result[i/2]+1;
+            }
+            else{
+                result[i]=result[i/2];
+            }
         }
         return result;
+
+
     }
 };
