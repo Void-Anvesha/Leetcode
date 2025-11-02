7eb348846b10eb4b74d071dac9c8da53bd5012e1
Void-Anvesha <anvesharastogi1717@gmail.com>
2025-11-02 22:35:22 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (81.00%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.cpp
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -3,9 +3,8 @@ public:
     int hammingWeight(int n) {
         int cnt=0;
         while(n>0){
-            //Unset the rightmmost set bit
-            n=(n&(n-1));
-            cnt++;
+            cnt+=(n%2);
+            n/=2;
         }
         return cnt;
     }
