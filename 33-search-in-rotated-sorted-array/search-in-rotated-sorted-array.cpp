a67b5e160aa50fef0e04068c9fff22c1a9da604c
Void-Anvesha <anvesharastogi1717@gmail.com>
2025-11-04 20:18:08 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (32.50%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,39 +1,27 @@
 class Solution {
 public:
-int search(vector<int> &nums, int target,int n)
-{
-    int low=0;
-    int high=n-1;
-    while(low<=high)
-    {
-        int mid=(low+high)/2;
+    int search(vector<int>& nums, int target) {
+    int n=nums.size(); 
+    int low=0,high=n-1;
+    int ans=-1;
+
+    while(low<=high){
+        int mid=low+(high-low)/2;
+        //If mid==target
         if(nums[mid]==target)return mid;
-         //Left sorted
-         else if(nums[mid]>=nums[low])
-         {
-             if(nums[low]<=target && target<=nums[mid])
-             {
-                high=mid-1;
-             }
-             else{
-                low=mid+1;
-             }
-         }
-         //Right sorted
-         else{
-            if(nums[mid]<=target && target<=nums[high])
-            {
-                low=mid+1;
-            }
-            else{
-                high=mid-1;
-            }
-         }
+        //Left Sorted
+        else if(nums[mid]>=nums[low]){
+            if(nums[low]<=target && target<=nums[mid])high=mid-1;
+            else low=mid+1;
+        }
+        //Right Sorted
+        else{
+            if(nums[mid]<=target && target<=nums[high])low=mid+1;
+            else high=mid-1;
+        }
 
     }
     return -1;
-}
-    int search(vector<int>& nums, int target) {
-        return search(nums,target,nums.size());
+       
     }
 };
