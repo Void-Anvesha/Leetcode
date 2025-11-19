8d70558ed23a83839939336171f02d932ae3a8be
Void-Anvesha <anvesharastogi1717@gmail.com>
2025-11-19 18:22:25 +0530

    Time: 38 ms (73.46%) | Memory: 128.4 MB (73.19%) - LeetSync

--- 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
@@ -1,33 +1,33 @@
 class Solution {
 private:
+
 void dfs(int node,vector<vector<int>>&adj,vector<int>&vis,int &nodes,int &degreeSum){
-    vis[node]=1;
-    nodes++;
-    degreeSum+=adj[node].size();
-    for(auto& neighbor:adj[node]){
-        if(!vis[neighbor]){
-          dfs(neighbor,adj,vis,nodes,degreeSum);
-        }
-    }
+vis[node]=1;
+nodes++;
+degreeSum+=adj[node].size();
+
+for(auto &it:adj[node]){
+    if(!vis[it])dfs(it,adj,vis,nodes,degreeSum);
+}
 }
 public:
     int countCompleteComponents(int n, vector<vector<int>>& edges) {
-        //Adjacency list
         vector<vector<int>>adj(n);
-        for(auto& edge:edges){
-            int u=edge[0];
-            int v=edge[1];
+        for(auto &edge:edges){
+            int v=edge[0];
+            int u=edge[1];
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
+
         int ans=0;
         vector<int>vis(n,0);
         for(int i=0;i<n;i++){
-            if(!vis[i]){
-                int nodes=0,degreeSum=0;
-                dfs(i,adj,vis,nodes,degreeSum);
-                if(degreeSum==(nodes*(nodes-1)))ans++;
-            }
+            int nodes=0,degreeSum=0;
+            dfs(i,adj,vis,nodes,degreeSum);
+            //Complete connected means 
+            //Alll nodes are connecting to each other
+            if(degreeSum==(nodes*(nodes-1)))ans++;
         }
         return ans;
     }
