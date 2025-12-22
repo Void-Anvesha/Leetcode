4e6a3c15b67226fd59fb6e53d9895dd9d79e9638
Void-Anvesha <anvesharastogi1717@gmail.com>
2025-12-22 18:21:23 +0530

    Time: 2 ms (67.89%) | Memory: 15.4 MB (96.70%) - LeetSync

--- 981-delete-columns-to-make-sorted/delete-columns-to-make-sorted.cpp
+++ 981-delete-columns-to-make-sorted/delete-columns-to-make-sorted.cpp
@@ -7,7 +7,9 @@ public:
         int k=strs[0].size();
        
         int cnt=0;
+        //Traversing each char of each word
         for(int i=0; i<k; i++){
+            //Trvaersing each word
             for(int j=1; j<n; j++){
                 if(strs[j][i] < strs[j-1][i]){
                     cnt++;
