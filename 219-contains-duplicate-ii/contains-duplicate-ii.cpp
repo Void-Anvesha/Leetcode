e104bf9ad1a8c2bc3a644eac56b29531e6845939
Void-Anvesha <anvesharastogi1717@gmail.com>
2025-12-24 11:48:49 +0530

    Time: 87 ms (20.55%) | Memory: 93.8 MB (81.27%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        int n=nums.size();
+        unordered_set<int>st;
+
+        int i=0 , j=0;
+
+        while(j<n){
+         //Shrink the window
+            if(abs(i-j) > k){
+                st.erase(nums[i]);
+                i++;
+            }
+
+        //Have you seen nums[j] in the past
+        if(st.find(nums[j]) != st.end()){
+            return true;
+        }
+        st.insert(nums[j]);
+        j++;
+        }
+        return false;
+    }
+};
