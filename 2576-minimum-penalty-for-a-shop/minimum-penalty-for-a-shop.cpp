c5073febf39b9e1b36e3d0789266ff8f776b420a
Void-Anvesha <anvesharastogi1717@gmail.com>
2025-12-26 19:57:22 +0530

    Time: 8 ms (55.08%) | Memory: 13.3 MB (91.48%) - LeetSync

--- 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.cpp
+++ 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.cpp
@@ -1,48 +1,26 @@
 class Solution {
 public:
     int bestClosingTime(string customers) {
+        
         int n= customers.size();
 
-        vector<int>prefix_N (n + 1 , 0);
-        prefix_N[0] = 0;
-
-        vector<int>suffixY(n + 1 , 0);
-        suffixY[n] =0;
-
-        for(int i=1; i<=n; i++){
-
-            if(customers[i-1] == 'N'){
-                prefix_N[i] = prefix_N[i-1] + 1;
-            }
-            else{
-                prefix_N[i] = prefix_N[i-1];
-            }
-        }
-
-
-        //Suffix
-        for(int i=n-1; i>=0; i--){
+        int minHour=0;
+        int penalty=count(customers.begin() , customers.end() , 'Y');
+        
+        int minPenalty=penalty;
+        for(int i=0; i<n; i++){
 
             if(customers[i] == 'Y'){
-                suffixY[i]=suffixY[i+1] + 1;
+                penalty--;
+            }else{
+                penalty++;
             }
-            else{
-                suffixY[i] = suffixY[i+1];
-            }
-        }
-
-
-        int minPenalty=INT_MAX;
-        int minHour=INT_MAX;
-
-        for(int i=0; i<=n; i++){
-
-            int currPenalty=prefix_N[i] + suffixY[i];
-            if(currPenalty < minPenalty){
-                minPenalty = currPenalty;
-                minHour = i;
+            if(penalty < minPenalty){
+                minPenalty=penalty;
+                minHour= i+1;
             }
         }
         return minHour;
+
     }
 };
