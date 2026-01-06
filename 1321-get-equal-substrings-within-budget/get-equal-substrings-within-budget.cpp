2b9280b34f321f9d1dc6d0ebd396b3c1148f8b13
Void-Anvesha <anvesharastogi1717@gmail.com>
2026-01-06 08:22:26 +0530

    Time: 1 ms (48.56%) | Memory: 10.3 MB (23.31%) - LeetSync

---
+++ 1321-get-equal-substrings-within-budget/get-equal-substrings-within-budget.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int equalSubstring(string s, string t, int maxCost) {
+        int n = s.length();
+        int i=0 , j=0;
+
+        int currCost=0;
+        int maxLen =0;
+//Each char in s is visited atmost twice
+//j->
+//i->
+//O(2*N)
+        while(j < n){
+            currCost+=abs(s[j] - t[j]);
+
+            while(currCost > maxCost){
+                currCost-=abs(s[i] - t[i]);
+                i++;
+            }
+            maxLen =max(maxLen , j-i+1);
+            j++;
+
+
+        }
+        return maxLen;
+    }
+};
