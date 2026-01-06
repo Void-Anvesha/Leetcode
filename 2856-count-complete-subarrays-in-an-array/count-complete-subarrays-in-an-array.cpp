38d04a01e0dc7c14c95fcc79eb63a395a974e30a
Void-Anvesha <anvesharastogi1717@gmail.com>
2026-01-06 12:04:53 +0530

    Time: 12 ms (91.57%) | Memory: 41.9 MB (60.21%) - LeetSync

---
+++ 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int countCompleteSubarrays(vector<int>& nums) {
+        int n = nums.size();
+        //Unique el
+        unordered_set<int>st(nums.begin() , nums.end());
+        int totalUnique = st.size();
+        int i=0 ,  j=0;
+        int cnt =0;
+       unordered_map<int,int>freq;
+        while(j < n){
+          freq[nums[j]]++;
+
+          while(freq.size() == totalUnique){
+            cnt+= (n - j);
+            freq[nums[i]]--;
+            if(freq[nums[i]] == 0)freq.erase(nums[i]);
+            i++;
+          }
+          j++;
+        }
+       return cnt;
+    }
+};
