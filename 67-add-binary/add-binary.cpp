0402981965b0aafa20c1da3fa06de6e6aef990db
Void-Anvesha <anvesharastogi1717@gmail.com>
2026-02-15 08:33:07 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (80.99%) - LeetSync

---
+++ 67-add-binary/add-binary.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        int m = a.length()-1;
+        int n = b.length()-1;
+
+        string res = "";
+
+        int sum = 0;
+        int carry = 0;
+
+        while( m >= 0 || n >= 0){
+
+            sum = carry;
+            if(m >= 0){
+                sum += a[m] - '0';
+                m--;
+            }
+
+            if( n >= 0){
+                sum += b[n] - '0';
+                n--;
+            }
+        
+        res += ((sum % 2 == 0) ? '0' : '1');
+        carry = (sum > 1) ? 1 : 0;
+
+
+        }
+
+        if(carry)res += '1';
+        reverse(res.begin() , res.end());
+        return res;
+    }
+};
