ff3598901473e4402a71fd65ae544cdaadd44176
Void-Anvesha <anvesharastogi1717@gmail.com>
2026-02-16 15:30:12 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (46.07%) - LeetSync

--- 190-reverse-bits/reverse-bits.cpp
+++ 190-reverse-bits/reverse-bits.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     int reverseBits(int n) {
-       int rev=0;
-       for(int i=0;i<=31;i++){
-           rev<<=1;
-           rev|=(n&1);
-           n>>=1;
-       }
-       return rev;
+        int rev = 0;
+        for(int i = 0; i <= 31; i++){
+            rev <<= 1;
+            //Get the LSB ( n & 1)
+            rev |= (n &1);
+            //Right shift to overcome the prev LSB
+            n>>=1;
+        }
+        return rev;
     }
 };
