a18b4222e406d7323849eff031bd7967b9145445
Void-Anvesha <anvesharastogi1717@gmail.com>
2026-02-23 08:49:09 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (21.99%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -11,19 +11,24 @@
  */
 class Solution {
 public:
-
-    void rightViewElements(TreeNode* root,int level,vector<int>& ans){
-        if(root==NULL) return;
-        if(ans.size()==level){
-            ans.push_back(root->val);
-        }
-        rightViewElements(root->right,level+1,ans);
-        rightViewElements(root->left,level+1,ans);
-        
-    }
     vector<int> rightSideView(TreeNode* root) {
-        vector<int> ans;
-        rightViewElements(root,0,ans);
-        return ans;
+        vector<int>res;
+        if(root == NULL)return res;
+        queue<TreeNode*>q;
+        q.push(root);
+
+        while(!q.empty()){
+            int size = q.size();
+            for(int i = 0; i < size; i++){
+                TreeNode* node = q.front();
+                q.pop();
+                //If it is the last node at this level
+                if(i == size-1)res.push_back(node->val);
+                if(node->left)q.push(node->left);
+                if(node->right)q.push(node->right);
+            }
+        }
+        return res;
+
     }
 };
