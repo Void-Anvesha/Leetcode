9102c03a94c762c41ee27ca79e5bf7c68c3b5033
Void-Anvesha <anvesharastogi1717@gmail.com>
2026-02-24 12:47:47 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (86.33%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-vector<int>dp;
+//Tabulation Method(Bottom-Up)
     int climbStairs(int n) {
-       if (dp.empty())              // initialize only once
-            dp.resize(n + 1, -1);
+        int prev2 = 1 , prev1 = 1;
         if(n == 0 || n == 1)return 1;
-        if(dp[n] != -1)return dp[n];
-        int left = climbStairs(n-1);
-        int right = climbStairs(n-2);
 
-        return dp[n] = left+right;
-
+        for(int i =2; i <= n; i++){
+            int curr = prev2 + prev1;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
     }
 };
