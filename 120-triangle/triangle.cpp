3a997462b78db114d0512c981f38bb0ebd98dcb7
Void-Anvesha <anvesharastogi1717@gmail.com>
2026-03-01 20:49:01 +0530

    Time: 3 ms (13.17%) | Memory: 12.8 MB (63.74%) - LeetSync

---
+++ 120-triangle/triangle.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int m , n;
+    int t[201][201];
+    int solve(vector<vector<int>>& triangle ,int row , int col){
+        if(row == m-1)return triangle[row][col];
+        if(t[row][col] != INT_MAX)return t[row][col];
+        int minPath = triangle[row][col] + min(solve(triangle , row+1 ,col) ,
+                    solve(triangle , row+1 , col+1));
+        return t[row][col] = minPath;
+    }
+    int minimumTotal(vector<vector<int>>& triangle) {
+        m = triangle.size();
+        n= triangle[0].size();
+        for(int i = 0; i < 201; i++){
+            for(int j =0; j < 201; j++){
+                t[i][j] = INT_MAX;
+            }
+        }
+       return solve(triangle , 0 , 0 );
+    
+    }
+};
